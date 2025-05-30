1e6e72b6947b54ff37fff3714e250a4079b0d106
Maaack <marek.belski@gmail.com>
2025-05-30 11:54:38 -0400

    Rsync with menus template.

--- addons/maaacks_ui_sound_controller/base/scripts/ui_sound_controller.gd
+++ addons/maaacks_ui_sound_controller/base/scripts/ui_sound_controller.gd
@@ -40,8 +40,13 @@ const MAX_DEPTH = 16
 @export var line_text_change_rejected : AudioStream
 
 @export_group("ItemList Sounds")
-@export var item_list_activated : AudioStream
 @export var item_list_selected : AudioStream
+@export var item_list_activated : AudioStream
+
+@export_group("Tree Sounds")
+@export var tree_item_selected : AudioStream
+@export var tree_item_activated : AudioStream
+@export var tree_button_clicked : AudioStream
 
 @onready var root_node : Node = get_node(root_path)
 
@@ -67,8 +72,11 @@ var line_text_change_rejected_player : AudioStreamPlayer
 var item_list_activated_player : AudioStreamPlayer
 var item_list_selected_player : AudioStreamPlayer
 
+var tree_item_activated_player : AudioStreamPlayer
+var tree_item_selected_player : AudioStreamPlayer
+var tree_button_clicked_player : AudioStreamPlayer
 
-func _update_persistent_signals():
+func _update_persistent_signals() -> void:
 	if not is_inside_tree():
 		return
 	var tree_node = get_tree()
@@ -79,7 +87,7 @@ func _update_persistent_signals():
 		if tree_node.node_added.is_connected(connect_ui_sounds):
 			tree_node.node_added.disconnect(connect_ui_sounds)
 
-func _build_stream_player(stream : AudioStream, stream_name : String = ""):
+func _build_stream_player(stream : AudioStream, stream_name : String = "") -> AudioStreamPlayer:
 	var stream_player : AudioStreamPlayer
 	if stream != null:
 		stream_player = AudioStreamPlayer.new()
@@ -89,55 +97,64 @@ func _build_stream_player(stream : AudioStream, stream_name : String = ""):
 		add_child(stream_player)
 	return stream_player
 
-func _build_button_stream_players():
+func _build_button_stream_players() -> void:
 	button_hovered_player = _build_stream_player(button_hovered, "ButtonHovered")
 	button_focused_player = _build_stream_player(button_focused, "ButtonFocused")
 	button_pressed_player = _build_stream_player(button_pressed, "ButtonClicked")
 
-func _build_tab_stream_players():
+func _build_tab_stream_players() -> void:
 	tab_hovered_player = _build_stream_player(tab_hovered, "TabHovered")
 	tab_changed_player = _build_stream_player(tab_changed, "TabChanged")
 	tab_selected_player = _build_stream_player(tab_selected, "TabSelected")
 
-func _build_slider_stream_players():
+func _build_slider_stream_players() -> void:
 	slider_hovered_player = _build_stream_player(slider_hovered, "SliderHovered")
 	slider_focused_player = _build_stream_player(slider_focused, "SliderFocused")
 	slider_drag_started_player = _build_stream_player(slider_drag_started, "SliderDragStarted")
 	slider_drag_ended_player = _build_stream_player(slider_drag_ended, "SliderDragEnded")
 
-func _build_line_stream_players():
+func _build_line_stream_players() -> void:
 	line_hovered_player = _build_stream_player(line_hovered, "LineHovered")
 	line_focused_player = _build_stream_player(line_focused, "LineFocused")
 	line_text_changed_player = _build_stream_player(line_text_changed, "LineTextChanged")
 	line_text_submitted_player = _build_stream_player(line_text_submitted, "LineTextSubmitted")
 	line_text_change_rejected_player = _build_stream_player(line_text_change_rejected, "LineTextChangeRejected")
 
-func _build_item_stream_players():
-	item_list_activated_player = _build_stream_player(item_list_activated,"ItemActivated")
-	item_list_selected_player = _build_stream_player(item_list_selected,"ItemSelected")
+func _build_item_list_stream_players() -> void:
+	item_list_activated_player = _build_stream_player(item_list_activated, "ItemActivated")
+	item_list_selected_player = _build_stream_player(item_list_selected, "ItemSelected")
 
-func _build_all_stream_players():
+func _build_tree_stream_players() -> void:
+	tree_item_activated_player = _build_stream_player(tree_item_activated, "TreeItemActivated")
+	tree_item_selected_player = _build_stream_player(tree_item_selected, "TreeItemSelected")
+	tree_button_clicked_player = _build_stream_player(tree_button_clicked, "TreeButtonClicked")
+
+func _build_all_stream_players() -> void:
 	_build_button_stream_players()
 	_build_tab_stream_players()
 	_build_slider_stream_players()
 	_build_line_stream_players()
-	_build_item_stream_players()
+	_build_item_list_stream_players()
+	_build_tree_stream_players()
 
-func _play_stream(stream_player : AudioStreamPlayer):
+func _play_stream(stream_player : AudioStreamPlayer) -> void:
 	if not stream_player.is_inside_tree():
 		return
 	stream_player.play()
 
-func _tab_event_play_stream(_tab_idx : int, stream_player : AudioStreamPlayer):
+func _tab_event_play_stream(_tab_idx : int, stream_player : AudioStreamPlayer) -> void:
 	_play_stream(stream_player)
 
-func _slider_drag_ended_play_stream(_value_changed : bool, stream_player : AudioStreamPlayer):
+func _slider_drag_ended_play_stream(_value_changed : bool, stream_player : AudioStreamPlayer) -> void:
 	_play_stream(stream_player)
 
-func _line_event_play_stream(_new_text : String, stream_player : AudioStreamPlayer):
+func _line_event_play_stream(_new_text : String, stream_player : AudioStreamPlayer) -> void:
 	_play_stream(stream_player)
 
 func _item_list_play_stream(_index : int, stream_player : AudioStreamPlayer) -> void:
+	_play_stream(stream_player)
+
+func _tree_button_clicked_play_stream(_tree_item : TreeItem, _column : int, _id : int, _mouse_button_index : int, stream_player : AudioStreamPlayer) -> void:
 	_play_stream(stream_player)
 
 func _connect_stream_player(node : Node, stream_player : AudioStreamPlayer, signal_name : StringName, callable : Callable) -> void:
@@ -167,6 +184,10 @@ func connect_ui_sounds(node: Node) -> void:
 	elif node is ItemList:
 		_connect_stream_player(node, item_list_activated_player, &"item_activated", _item_list_play_stream)
 		_connect_stream_player(node, item_list_selected_player, &"item_selected", _item_list_play_stream)
+	elif node is Tree:
+		_connect_stream_player(node, tree_item_activated_player, &"item_activated", _play_stream)
+		_connect_stream_player(node, tree_item_selected_player, &"item_selected", _play_stream)
+		_connect_stream_player(node, tree_button_clicked_player, &"button_clicked", _tree_button_clicked_play_stream)
 
 func _recursive_connect_ui_sounds(current_node: Node, current_depth : int = 0) -> void:
 	if current_depth >= MAX_DEPTH:
@@ -180,7 +201,7 @@ func _ready() -> void:
 	_recursive_connect_ui_sounds(root_node)
 	persistent = persistent
 
-func _exit_tree():
+func _exit_tree() -> void:
 	var tree_node = get_tree()
 	if tree_node.node_added.is_connected(connect_ui_sounds):
 		tree_node.node_added.disconnect(connect_ui_sounds)
